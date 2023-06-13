3f1be4611ccfe742c9ab11796e69ab552a937bf8
danstepanov <danhstepanov@gmail.com>
2023-06-12 18:40:29 -0700

    Initial commit
    Generated by create-expo-stack 0.1.0.
    
    Using with-typescript-expo-router-tab template.

---
+++ app/modal.tsx
@@ -0,0 +1,21 @@
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+
+export default function ModalScreen() {
+  return (
+    <View className={styles.container}>
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      <Text className={styles.title}>Modal</Text>
+      <View className={styles.separator} />
+      <EditScreenInfo path="app/modal.tsx" />
+    </View>
+  );
+}
+
+const styles = {
+  container: `items-center flex-1 justify-center`,
+  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
+  title: `text-xl font-bold`
+};
