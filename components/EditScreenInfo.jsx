88c34373137090d0173e98a3a1f928f18bbf5cec
danstepanov <danhstepanov@gmail.com>
2023-06-12 18:40:29 -0700

    Initial commit
    Generated by create-expo-stack 0.0.13.

---
+++ components/EditScreenInfo.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Colors from '../constants/Colors';
+import { ExternalLink } from './ExternalLink';
+import { MonoText } from './StyledText';
+import { Text, View } from './Themed';
+
+export default function EditScreenInfo({ path }) {
+  return (
+    <View>
+      <View className="items-center mx-12">
+        <Text
+          className="text-lg text-center"
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Open up the code for this screen:
+        </Text>
+
+        <View
+          className="my-2 rounded-md px-1"
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>{path}</MonoText>
+        </View>
+
+        <Text
+          className="text-lg text-center"
+          lightColor="rgba(0,0,0,0.8)"
+          darkColor="rgba(255,255,255,0.8)">
+          Change any of the text, save the file, and your app will automatically update.
+        </Text>
+      </View>
+
+      <View className="mt-4 mx-5 flex items-center">
+        <ExternalLink
+          className="py-4"
+          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
+          <Text className="text-center" lightColor={Colors.light.tint}>
+            Tap here if your app doesn't automatically update after making changes
+          </Text>
+        </ExternalLink>
+      </View>
+    </View>
+  );
+}
